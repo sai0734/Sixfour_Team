d630f8a33ad0d23893a9a9d9966184bb2c90508a
dyddlr068 <dyddlr068@gmail.com>
2026-07-03 11:22:49 +0900

    260703_11:22_오류수정

--- wedding_back/src/main/java/com/wedding/WeddingApplication.java
+++ wedding_back/src/main/java/com/wedding/WeddingApplication.java
@@ -2,18 +2,10 @@ package com.wedding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-
-
-
 
 @SpringBootApplication
 
-=======
 
-@SpringBootApplication
->>>>>>> 70c73be166854e72c3573de7861da3374a964347
 public class WeddingApplication {
 
 	public static void main(String[] args) {
