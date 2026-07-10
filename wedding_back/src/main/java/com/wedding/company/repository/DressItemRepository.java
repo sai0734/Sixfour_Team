4c41206cb4a200c0709a1c4c7898c986e4464934
DESKTOP-OT238HQ\EZEN <ysjin1226@naver.com>
2026-07-10 16:18:40 +0900

    260710_16:18_admin_companypage

--- wedding_back/src/main/java/com/wedding/company/repository/DressItemRepository.java
+++ wedding_back/src/main/java/com/wedding/company/repository/DressItemRepository.java
@@ -3,8 +3,14 @@ package com.wedding.company.repository;
 import com.wedding.company.domain.DressItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DressItemRepository extends JpaRepository<DressItem, Long> {
   List<DressItem> findByCompany_CmnoOrderByOrdAsc(Long cmno);
-  void deleteByCompany_Cmno(Long cmno);
+
+  @Modifying
+  @Query("DELETE FROM DressItem d WHERE d.company.cmno = :cmno")
+  void deleteByCompany_Cmno(@Param("cmno") Long cmno);
 }
