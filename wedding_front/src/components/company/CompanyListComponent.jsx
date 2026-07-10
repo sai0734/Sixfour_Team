0d2f52175eade912de41ae03a34083ffbbcd7c52
DESKTOP-OT238HQ\EZEN <ysjin1226@naver.com>
2026-07-10 09:05:03 +0900

    260710_09:04_mainPage modify

--- wedding_front/src/components/company/CompanyListComponent.jsx
+++ wedding_front/src/components/company/CompanyListComponent.jsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { deleteOne, getCompanyImageUrl, getList } from "../../api/companyApi";
